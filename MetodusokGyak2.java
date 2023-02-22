9fecaba714676f5488ae84bb6bed37e49f0e4b14
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2023-02-22 15:53:11 +0100

    Equalizer

--- MetodusokGyak2.java
+++ MetodusokGyak2.java
@@ -1,17 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.metodusokgyak2;
 
-/**
- *
- * @author Kelo
- */
 public class MetodusokGyak2 {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        osszesen();
+    
+    
     }
-}
+    
+    private static void osszesen() {
+        elso();
+        masodik();
+        harmadik();
+    }
+
+    private static void elso() {
+        int osszeg = Elso10SzamOsszege();
+        String s = String.format("Az első 10 szám összege: " + osszeg);
+        kiir(s);
+    }
+
+
+    
+    private static void masodik() {
+        int a = 3, b = 6, c = 7, d = 9;
+        int osszeg = negySzam(a, b, c, d);
+        String s = String.format("%d + %d + %d + %d = %d", a, b, c, d, osszeg);
+        kiir(s);
+    }
+
+     private static void harmadik() {
+        int a = 3, b = 6;
+        int osszeg = ketSzam(a, b);
+        String s = String.format("%d + %d = %d", a, b, osszeg);
+        kiir(s);
+    }
+    
+    
+    
+    private static int Elso10SzamOsszege() {
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += 1;
+        }
+        return osszeg;
+    }
+    
+    private static int ketSzam(int a, int b) {
+        return a + b;
+    }
+    
+    
+    private static int negySzam(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+    
+    
+    private static void kiir(String str){
+        System.out.println(str);
+    }
+}   
