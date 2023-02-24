38af8c3b5b83b91f7653f796031bca953b031584
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2023-02-24 12:02:13 +0100

    secondEqualizer

---
+++ Equalizer.java
@@ -0,0 +1,40 @@
+package com.mycompany.metodusokgyak2;
+
+import java.util.Random;
+
+public class Equalizer {
+    
+    private static  Random rnd = new Random();
+    
+    public static void main(String[] args) {
+        equalizer_5_Sorbol();
+        
+    }
+    private static void equalizer_5_Sorbol(){
+          eq();
+          eq(12);
+          eq();
+          eq(8);
+          eq();
+        
+                
+        
+    
+    }
+    private static void eq() {
+        int db = rnd.nextInt(3,8);
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.print(szin + " ");
+        }        
+        System.out.println("");
+    }
+    private static void eq(int hossz){
+        int db = rnd.nextInt(3,8);
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.print(szin + " ");
+        }        
+        System.out.println("");
+    }
+}   
