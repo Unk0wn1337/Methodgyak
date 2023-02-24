a301d87d38d0b205388fafc30662cf74cf7de600
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2023-02-24 13:22:34 +0100

    qrkodcommentnelkul

--- QRKOD.java
+++ QRKOD.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.metodusokgyak2;
 
-/**
- *
- * @author Kelo
- */
+
 public class QRKOD {
     
+    public static void main(String[] args) {
+        
+    }
+
+
+
+
+
 }
