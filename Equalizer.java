203979873835c2f669d31c0c5ee469c264a7c9c5
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2023-02-24 12:32:39 +0100

    negyedik

--- Equalizer.java
+++ Equalizer.java
@@ -14,16 +14,16 @@ public class Equalizer {
           eq();
           eq(12);
           eq();
-          eq(8);
-          eq();
+          eq(8, true);
+          eq(true);
         
                 
         
     
     }
     private static void eq() {
-        int db = rnd.nextInt(3,8);
-        eq(db, false);           
+        
+        eq(false);           
         
     }
     private static void eq(boolean kiir) {
@@ -32,18 +32,17 @@ public class Equalizer {
         
     }
     private static void eq(int hossz) {
-        int db = rnd.nextInt(3,8);
+        
         eq(hossz, false);           
         
     }
     private static void eq(int hossz,boolean kiir){
-        int db = rnd.nextInt(3,8);
         String szin = "\u001B[45m";
-        for (int i = 0; i < db; i++) {
-            System.out.print(szin + " ");
+        for (int i = 0; i < hossz; i++) {
+            System.out.print(szin + "*");
         }        
-        if (kiir)
-            System.out.println("\u001B[40m"+(hossz));
+        if(kiir)
+            System.out.print("\u001B[40m"+"("+(hossz)+")");
         
         System.out.println("");
     }
